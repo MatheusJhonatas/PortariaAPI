7e9e881256c351b571a3d63689812df4d1b611f6
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-01-08 14:57:08 -0300

    add atributes class

---
+++ Models/PrestadorServico.cs
@@ -0,0 +1,8 @@
+using PortariaAPI.Models;
+
+
+public class PrestadorServico : Visitante
+{
+    public string empresa { get; set; }
+
+}
--- Models/Visitantes.cs
+++ Models/Visitantes.cs
@@ -5,4 +5,8 @@ public class Visitante
     public string Nome { get; set; }
     public string DocumentoRG { get; set; }
     public string Tipo { get; set; }
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+    }
 }
