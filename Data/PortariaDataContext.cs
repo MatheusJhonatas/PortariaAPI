6650c1e0d5b00845cb4d504c7f432fe4bb5efdec
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-01-11 13:07:09 -0300

    add dbContext

--- Data/PortariaDataContext.cs
+++ Data/PortariaDataContext.cs
@@ -1,6 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using PortariaAPI.Models;
 
 public class PortariaDataContext : DbContext
 {
+    public DbSet<Visitante> Visitantes { get; set; }
+    public DbSet<Registro> Registros { get; set; }
+    public DbSet<PrestadorServico> PrestadorServicos { get; set; }
+    // public DbSet<FuncionarioPortaria> FuncionarioPortarias { get; set; }
+    // public DbSet<Portaria> Portarias { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Portaria;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;");
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
+builder.Services.AddDbContext<PortariaDataContext>();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
