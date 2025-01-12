a67c6063565020a2f519bf413ee667b43ade9a6a
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-01-12 11:41:23 -0300

    add IPessoa

---
+++ Models/Endereço.cs
@@ -0,0 +1,5 @@
+public class Endereco
+{
+    public string Rua { get; set; }
+    public string NumeroCasa { get; set; }
+}
---
+++ Models/Morador.cs
@@ -0,0 +1,7 @@
+using PortariaAPI.Models;
+
+public class Morador
+{
+    public Endereco endereco { get; set; }
+
+}
