78dfc98c7f12c92e170909b3790b96b99a108ca0
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-01-13 16:29:15 -0300

    add new atributes

--- Interface/IPessoa.cs
+++ Interface/IPessoa.cs
@@ -4,4 +4,6 @@ public interface IPessoa
     public string Nome { get; set; }
     public string DocumentoRG { get; set; }
     public string Endereco { get; set; }
+    public string Tipo { get; set; }
+
 }
--- Models/PrestadorServico.cs
+++ Models/PrestadorServico.cs
@@ -4,7 +4,9 @@ using PortariaAPI.Models;
 public class PrestadorServico : IPessoa
 {
     public string empresa { get; set; }
+
     public string Nome { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     public string DocumentoRG { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     public string Endereco { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string Tipo { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 }
