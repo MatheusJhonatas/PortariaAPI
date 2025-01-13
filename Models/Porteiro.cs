f7ae68f2e8c7c3310f7fa021fb423bba5edebe7b
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-01-13 17:38:05 -0300

    Mapping Morador

--- Models/Porteiro.cs
+++ Models/Porteiro.cs
@@ -4,7 +4,7 @@ using PortariaAPI.Models;
 public class Porteiro : IPessoa
 {
     public List<Registro> Registros { get; set; }
-    public List<FuncionarioPortaria> FuncionariosPortarias { get; set; }
+
     public string Nome { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     public string DocumentoRG { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     public string Endereco { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
