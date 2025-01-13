12fa9105c5e5c8c989b7d761ab3982bcd85c3607
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-01-13 16:37:35 -0300

    add class mappings

--- Data/PortariaDataContext.cs
+++ Data/PortariaDataContext.cs
@@ -3,14 +3,24 @@ using PortariaAPI.Models;
 
 public class PortariaDataContext : DbContext
 {
+    public DbSet<Porteiro> Porteiros { get; set; }
+    public DbSet<Morador> Moradores { get; set; }
     public DbSet<Visitante> Visitantes { get; set; }
     public DbSet<Registro> Registros { get; set; }
     public DbSet<PrestadorServico> PrestadorServicos { get; set; }
-    // public DbSet<FuncionarioPortaria> FuncionarioPortarias { get; set; }
-    // public DbSet<Portaria> Portarias { get; set; }
+
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Portaria;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // modelBuilder.ApplyConfiguration(new PorteirosMap());
+        // modelBuilder.ApplyConfiguration(new MoradoresMap());
+        // modelBuilder.ApplyConfiguration(new VisitantesMap());
+        // modelBuilder.ApplyConfiguration(new RegistrosMap());
+        // modelBuilder.ApplyConfiguration(new PrestadorServicosMap());
+
     }
 }
