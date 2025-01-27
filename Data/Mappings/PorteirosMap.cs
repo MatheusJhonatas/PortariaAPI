d049caa08d8f5d4592fc641a98b4f8ecdbef3472
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-01-27 20:51:40 -0300

    Complemento map morador

---
+++ Data/Mappings/PorteirosMap.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class PorteirosMap : IEntityTypeConfiguration<Porteiro>
+{
+    public void Configure(EntityTypeBuilder<Porteiro> builder)
+    {
+        builder.ToTable("Porteiro");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+        .ValueGeneratedOnAdd()
+        .UseIdentityColumn();
+
+        builder.Property(x => x.Nome)
+        .IsRequired()
+        .HasColumnName("Nome")
+        .HasColumnType("NVARCHAR")
+        .HasMaxLength(100);
+
+    }
+}
