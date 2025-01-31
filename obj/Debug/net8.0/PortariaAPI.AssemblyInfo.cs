2ad1feeb8fd55bd150f05450c68e32d547c80a9e
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-01-30 22:20:10 -0300

    cache

--- obj/Debug/net8.0/PortariaAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortariaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortariaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa1f842452278d8a15383c831ceb53e7d024034")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d049caa08d8f5d4592fc641a98b4f8ecdbef3472")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortariaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortariaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
